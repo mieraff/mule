3eb9079072ecd6f19da0e471edaef42ded3e7766
Nicolas Santiago Di Nucci <nico.dinu@gmail.com>
2021-01-11 20:01:58 -0300

    More progress

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/resolver/SdkCompletionCallbackArgumentResolver.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/resolver/SdkCompletionCallbackArgumentResolver.java
@@ -6,26 +6,29 @@
  */
 package org.mule.runtime.module.extension.internal.runtime.resolver;
 
-
 import static org.mule.runtime.module.extension.api.runtime.privileged.ExecutionContextProperties.COMPLETION_CALLBACK_CONTEXT_PARAM;
 
 import org.mule.runtime.extension.api.runtime.operation.ExecutionContext;
-import org.mule.runtime.extension.api.runtime.process.CompletionCallback;
 import org.mule.runtime.module.extension.api.runtime.privileged.ExecutionContextAdapter;
+import org.mule.runtime.module.extension.internal.runtime.execution.adapter.SdkCompletionCallbackAdapter;
+import org.mule.sdk.api.runtime.process.CompletionCallback;
 
 /**
- * {@link ArgumentResolver} which returns the
- * {@link org.mule.runtime.module.extension.api.runtime.privileged.ExecutionContextProperties#COMPLETION_CALLBACK_CONTEXT_PARAM}
+ * ADD JDOC
+ * {@link ArgumentResolver} which returns the {@link org.mule.runtime.module.extension.api.runtime.privileged.ExecutionContextProperties#COMPLETION_CALLBACK_CONTEXT_PARAM}
  * context variable.
  * <p/>
  * Notice that this resolver only works if the {@link ExecutionContext} is a {@link ExecutionContextAdapter}
  *
  * @since 4.0
  */
-public final class CompletionCallbackArgumentResolver implements ArgumentResolver<CompletionCallback> {
+public final class SdkCompletionCallbackArgumentResolver implements ArgumentResolver<CompletionCallback> {
 
   @Override
   public CompletionCallback resolve(ExecutionContext executionContext) {
-    return (CompletionCallback) ((ExecutionContextAdapter) executionContext).getVariable(COMPLETION_CALLBACK_CONTEXT_PARAM);
+    org.mule.runtime.extension.api.runtime.process.CompletionCallback completionCallback =
+        (org.mule.runtime.extension.api.runtime.process.CompletionCallback) ((ExecutionContextAdapter) executionContext)
+            .getVariable(COMPLETION_CALLBACK_CONTEXT_PARAM);
+    return completionCallback == null ? null : new SdkCompletionCallbackAdapter(completionCallback);
   }
 }

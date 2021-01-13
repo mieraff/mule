0f654eaa5b44c93a06edebef2996eea19b9f1a4c
Nicolas Santiago Di Nucci <nico.dinu@gmail.com>
2021-01-12 23:57:40 -0300

    more progress

--- modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/execution/OperationTransactionalActionArgumentResolver.java
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/execution/OperationTransactionalActionArgumentResolver.java
@@ -13,17 +13,17 @@ import org.mule.runtime.extension.api.tx.OperationTransactionalAction;
 import org.mule.runtime.module.extension.internal.runtime.resolver.ArgumentResolver;
 
 /**
- * {@link ArgumentResolver} implementation for {@link OperationTransactionalAction} parameters
+ * ADD JDOC {@link ArgumentResolver} implementation for {@link OperationTransactionalAction} parameters
  *
  * @since 4.0
  */
-public class OperationTransactionalActionArgumentResolver implements ArgumentResolver<OperationTransactionalAction> {
+public class OperationTransactionalActionArgumentResolver implements ArgumentResolver<Object> {
 
   /**
    * {@inheritDoc}
    */
   @Override
-  public OperationTransactionalAction resolve(ExecutionContext executionContext) {
-    return (OperationTransactionalAction) executionContext.getParameter(TRANSACTIONAL_ACTION_PARAMETER_NAME);
+  public Object resolve(ExecutionContext executionContext) {
+    return executionContext.getParameter(TRANSACTIONAL_ACTION_PARAMETER_NAME);
   }
 }
